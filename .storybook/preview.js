4b12e91de7562b9bb903ba32f9621d219e5826f6
Nicole Konegen <konegen@radikant.com>
2022-05-06 15:23:11 +0200

    feat(Link): change icons

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,8 @@
 import { createElement, useEffect } from "react";
 import { run as runHolder } from "holderjs/holder";
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faStar, faCoffee, faMapMarkerAlt, faChartPie, faBell, faCalendar, faArrowDown, faArrowRight } from '@fortawesome/free-solid-svg-icons'
-import { faInfoCircle, faCheckCircle, faTriangleExclamation, faCircleExclamation, faXmark, faCheck, faChevronDown, faChevronLeft, faChevronsLeft, faChevronRight, faChevronsRight, faMagnifyingGlass, faUser, faGlobe, faShoppingCart, faCompass, faChartColumn, faHouse, faGaugeMin } from '@fortawesome/pro-light-svg-icons'
+import { faStar, faCoffee, faMapMarkerAlt, faChartPie, faBell, faCalendar } from '@fortawesome/free-solid-svg-icons'
+import { faInfoCircle, faCheckCircle, faTriangleExclamation, faCircleExclamation, faXmark, faCheck, faChevronDown, faChevronLeft, faChevronsLeft, faChevronRight, faChevronsRight, faMagnifyingGlass, faUser, faGlobe, faShoppingCart, faCompass, faChartColumn, faHouse, faGaugeMin, faArrowRight } from '@fortawesome/pro-light-svg-icons'
 import { faFacebook, faInstagram, faLinkedinIn, faTwitter, faXing, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import "./index.scss";
 
@@ -32,7 +32,6 @@ library.add(
   faChartPie,
   faBell,
   faCalendar,
-  faArrowDown,
   faArrowRight,
   faFacebook,
   faInstagram,
