822b31dcbba2ce3ec863860fd9020e427b821bb9
Nicole Konegen <konegen@radikant.com>
2022-05-27 09:37:56 +0200

    feat(Modal): add Alert Modal sub-component

--- src/modal/Modal.stories.jsx
+++ src/modal/Modal.stories.jsx
@@ -3,6 +3,7 @@ import { Button } from "../button/Button";
 import { Input } from "../form-elements/Input";
 import { Textarea } from "../form-elements/Textarea";
 import BsModal from "react-bootstrap/Modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default {
   title: "Components/Modal",
@@ -11,7 +12,12 @@ export default {
 
 const DefaultTemplate = (args) => {
   const DemoComponent = ModalDemo(
-    <>
+    <BsModal.Dialog>
+      <BsModal.Header>
+        <div className="modal-title">Modal title</div>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </BsModal.Header>
+
       <BsModal.Body>
         <p>Modal Content</p>
       </BsModal.Body>
@@ -20,7 +26,7 @@ const DefaultTemplate = (args) => {
         <Button variant="outline" color="primary" size="s" data-bs-dismiss="modal">Button</Button>
         <Button size="s">Button</Button>
       </BsModal.Footer>
-    </>
+    </BsModal.Dialog>
   )
   return (
     <DemoComponent {...args}></DemoComponent>
@@ -29,7 +35,12 @@ const DefaultTemplate = (args) => {
 
 const MessageTemplate = (args) => {
   const DemoComponent = ModalDemo(
-    <>
+    <BsModal.Dialog>
+      <BsModal.Header>
+        <div className="modal-title">Modal title</div>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </BsModal.Header>
+
       <BsModal.Body className="ifx__modal-message">
         <Input labelPosition="top" label="Label"></Input>
         <Textarea label="Default"></Textarea>
@@ -39,7 +50,7 @@ const MessageTemplate = (args) => {
         <Button variant="outline" color="primary" size="s" data-bs-dismiss="modal">Button</Button>
         <Button size="s">Button</Button>
       </BsModal.Footer>
-    </>
+    </BsModal.Dialog>
   )
   return (
     <DemoComponent {...args}></DemoComponent>
@@ -48,11 +59,44 @@ const MessageTemplate = (args) => {
 
 const SmallTemplate = (args) => {
   const DemoComponent = ModalDemo(
-    <>
+    <BsModal.Dialog>
+      <BsModal.Header>
+        <div className="modal-title">Modal title</div>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </BsModal.Header>
+
       <BsModal.Body>
         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
       </BsModal.Body>
-    </>
+    </BsModal.Dialog>
+  )
+  return (
+    <DemoComponent {...args}></DemoComponent>
+  )
+}
+
+const AlertTemplate = (args) => {
+  const DemoComponent = ModalDemo(
+    <BsModal.Dialog className="ifx__modal-alert">
+      <div className="ifx__modal-alert-wrapper">
+        <FontAwesomeIcon icon={["fal", "circle-exclamation"]} className="ifx__modal-alert-icon"/>
+      </div>
+      <div>
+        <BsModal.Header>
+          <div className="modal-title">Alert message</div>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </BsModal.Header>
+
+        <BsModal.Body>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</p>
+        </BsModal.Body>
+
+        <BsModal.Footer>
+          <Button variant="outline" color="primary" size="s" data-bs-dismiss="modal">Button</Button>
+          <Button size="s">Button</Button>
+        </BsModal.Footer>
+      </div>
+    </BsModal.Dialog>
   )
   return (
     <DemoComponent {...args}></DemoComponent>
@@ -64,3 +108,5 @@ export const Default = DefaultTemplate.bind({});
 export const Message = MessageTemplate.bind({});
 
 export const Small = SmallTemplate.bind({});
+
+export const Alert = AlertTemplate.bind({});
--- src/modal/ModalDemo.jsx
+++ src/modal/ModalDemo.jsx
@@ -1,4 +1,3 @@
-import BsModal from "react-bootstrap/Modal";
 import { Button } from "../button/Button";
 import { useRef, useEffect } from "react";
 import { Modal as ModalScript } from "bootstrap";
@@ -6,13 +5,7 @@ import { Modal as ModalScript } from "bootstrap";
 export const Modal = ({ children }) => {
   return (
     <div className="modal fade" id="exampleModal" tabIndex="-1" aria-hidden="true">
-      <BsModal.Dialog>
-        <BsModal.Header>
-          <div className="modal-title">Modal title</div>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </BsModal.Header>
-        {children}
-      </BsModal.Dialog>
+      {children}
     </div>
   )
 };
