87e4e5cff07ceb836cd0f1474667c7638218ca15
Maximilian Gunser <gunser@radikant.com>
2022-06-01 16:38:46 +0200

    Add correct Infineon Logo to storybook

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -35,5 +35,5 @@ export default create({
 
   brandTitle: 'Infineon Technologies AG',
   brandUrl: 'https://infineon.com',
-  brandImage: 'https://www.infineon.com/frontend/release_2022-03-1/dist/resources/img/logo-desktop-en.png',
+  brandImage: 'static/media/public/Logo.svg',
 });
