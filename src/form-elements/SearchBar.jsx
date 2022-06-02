cee0fc5127eef2651f5518b21d4a7cbbe2e5fd54
Maximilian Gunser <gunser@radikant.com>
2022-06-02 11:38:41 +0200

    Refactoring for searchbar, select and textarea

--- src/form-elements/SearchBar.jsx
+++ src/form-elements/SearchBar.jsx
@@ -1,7 +1,5 @@
 import PropTypes from "prop-types";
 import BsForm from "react-bootstrap/Form";
-import BsInputGroup from "react-bootstrap/InputGroup";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const propTypes = {
   /** Specifies a large or small Input field */
@@ -9,21 +7,24 @@ const propTypes = {
 
   /** Set a disabled Input field */
   disabled: PropTypes.bool,
+
+  /** Set an Input field state */
+  state: PropTypes.oneOf(["default", "success", "error"]),
 };
 
-export const SearchBar = ({ size, ...props }) => {
+export const SearchBar = ({ size, state, ...props }) => {
   const bsSize =
     size === "s" ? "sm" 
     : size === "l" ? "lg" 
     : undefined;
 
+  const stateClass =
+    state === "success" ? "is-valid"
+    : state === "error" ? "is-invalid"
+    : "";
+
   return (
-    <BsInputGroup>
-      <BsForm.Control id="ifx__inputSearch" type="text" placeholder="Search" size={bsSize} {...props}></BsForm.Control>
-      <span class="ifx__search-icon-wrapper">
-        <FontAwesomeIcon icon={["fal", "magnifying-glass"]}/>
-      </span>
-    </BsInputGroup>    
+    <BsForm.Control className={"ifx__searchbar " + stateClass} type="text" placeholder="Search" size={bsSize} {...props}></BsForm.Control>
   );
 };
 SearchBar.propTypes = propTypes;
