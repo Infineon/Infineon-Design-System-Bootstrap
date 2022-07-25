b2d279dbe764fdac8d4aa076e1f0a97941f57aa9
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-07-25 19:32:58 +0200

    fix: use correct markup for navbar

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import "bootstrap";
 
-import { initMobileNav } from "./nav-bar/mobile-nav";
 import { initNavBar, initVerticalNavBar } from "./nav-bar/nav-bar";
+import { initMobileNav } from "./nav-bar/mobile-nav";
 import { initBackToTopButton } from "./button-back-to-top/button-back-to-top";
 import {
   initSelect
 } from "./form-elements/SelectElement";
 
-setTimeout(( ) => {
-  initMobileNav();
-  initNavBar();
-  initVerticalNavBar();
-  initBackToTopButton();
-  initSelect();
-}, 100)
+initMobileNav();
+initNavBar();
+initVerticalNavBar();
+initBackToTopButton();
+initSelect();
+initBackToTopButton();
+initSelect();
--- src/nav-bar/NavBar.stories.jsx
+++ src/nav-bar/NavBar.stories.jsx
@@ -29,7 +29,7 @@ const JsWrapper = ({ children }) => {
     initMobileNav();
     initNavBar();
     initVerticalNavBar();
-  }, [])
+  }, []);
 
   return (
     <>
