9106d443ca509a049b588b82ee1414c0c82caffa
tishoyanchev <tihomir.yanchev-EE@infineon.com>
2022-07-26 16:05:56 +0200

    carousel updated controls + new hero banner

--- src/carousel/Carousel.stories.jsx
+++ src/carousel/Carousel.stories.jsx
@@ -12,10 +12,23 @@ export default {
 };
 
 const DefaultTemplate = (args) => (
-  <Carousel {...args}></Carousel>
-  // <h6>Image Headline</h6>
-  // <p>Further Description of the Image of Graphic</p>
+  <>
+    <Carousel {...args}></Carousel>
+    <h6 className="mb-0 mt-3">Image Headline</h6>
+    <p className="m-0">Further Description of the Image of Graphic</p>
+  </>
+);
+
+const HeroBannerTemplate = (args) => (
+  <Carousel {...args}>
+    <BsCarousel.Caption>
+      <div>
+        <h3>Slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </BsCarousel.Caption>
+  </Carousel>
 );
 
 export const Default = DefaultTemplate.bind({});
-
+export const HeroBanner = HeroBannerTemplate.bind({});
